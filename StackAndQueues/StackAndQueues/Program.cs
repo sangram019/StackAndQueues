252ba6d0b6d03698292318080dfb8d058a0b7518
Sangram <sangram15.behera@gmail.com>
2022-05-26 01:41:19 +0530

    StackAndQueues

---
+++ StackAndQueues/StackAndQueues/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LinkedListStackAndQueue
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------Welcome to Linked List Program----------");
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
